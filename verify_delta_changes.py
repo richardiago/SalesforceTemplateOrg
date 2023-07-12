5705fb61ca8c30122a2af829b6676a54feac235b
Iago Richard <32197319+richardiago@users.noreply.github.com>
2023-07-12 15:43:01 -0300

    Update verify_delta_changes.py

--- verify_delta_changes.py
+++ verify_delta_changes.py
@@ -1,9 +1,11 @@
 import sys
 import os
+import subprocess
+
 
 if __name__ == "__main__":
    
    original_branch = sys.argv[1]
    destination_branch = sys.argv[2]
-   
-   os.system("git diff --name-only " + original_branch + " " + destination_branch)
+
+   subprocess.run(["git", "diff --name-only " + original_branch + " " + destination_branch])
