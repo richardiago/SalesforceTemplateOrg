df330f7b74bbc7f02b9c116bd14015b84cd527f9
Iago Richard <32197319+richardiago@users.noreply.github.com>
2023-07-14 17:16:48 -0300

    new changes

--- verify_delta_changes.py
+++ verify_delta_changes.py
@@ -1,15 +1,28 @@
+import os
 import sys
 import subprocess
 
+def verifyDeltaChages(origin_branch, destination_branch):
+
+   # Get the list of files that have changed
+   result = subprocess.run(
+      ["git", "diff", "--name-only", origin_branch, destination_branch],
+      stdout=subprocess.PIPE,
+      text=True
+   )
+
+   # Concatenate the files into a single string
+   files       = result.stdout.split('\n')
+   concatFiles = ''
+
+   for file in files:
+     if os.path.exists(file):
+        concatFiles += ' ' + file
+
+   return concatFiles
+
 if __name__ == "__main__":
    
-   origin_branch = sys.argv[1]
-   destination_branch = sys.argv[2]
+   verifyDeltaChages(sys.argv[1], sys.argv[2])
 
-   result = subprocess.run(
-        ["git", "diff", "--name-only", origin_branch, destination_branch],
-        stdout=subprocess.PIPE,
-        text=True
-   )
    
-   print(result.stdout)
