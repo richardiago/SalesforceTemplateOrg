842d86a1b54ab6452c8b07f10a8c85377edddf6b
Iago Richard <iagorichard@outlook.com>
2023-07-15 20:35:35 -0300

    Update verify_delta_changes.py

--- verify_delta_changes.py
+++ verify_delta_changes.py
@@ -5,7 +5,7 @@ import subprocess
 def verifyDeltaChages(origin_branch, destination_branch):
 
    # Fetch the latest changes
-   subprocess.run("git fetch --all", shell=True)
+   subprocess.run("git fetch", shell=True)
 
    # Get the list of files that have changed
    result = subprocess.run(
@@ -26,6 +26,6 @@ def verifyDeltaChages(origin_branch, destination_branch):
 
 if __name__ == "__main__":
    
-   verifyDeltaChages(sys.argv[1], sys.argv[2])
+   verifyDeltaChages('origin/feature/teste-automacao', 'origin/dev')
 
    
