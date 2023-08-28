9333e1ffe7d051d7a70c584219574751ce729560
Iago Richard <32197319+richardiago@users.noreply.github.com>
2023-08-28 11:19:11 -0300

    Validation name action

---
+++ validate_filename.py
@@ -0,0 +1,21 @@
+import re
+import sys
+import subprocess
+
+def validateFilename(origin_branch):
+
+   output = subprocess.check_output(["git", "diff", "--name-only", "HEAD^", "HEAD"])
+   files_changed = output.decode("utf-8").splitlines()
+
+   # Check if any of the changed files match the pattern
+   pattern = r"^[^_]*\.field-meta\.xml$"
+
+   for file_name in files_changed:
+    if not re.match(pattern, file_name):
+        print(f"File name '{file_name}' does not match the required pattern")
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+   
+   validateFilename(sys.argv[1])
